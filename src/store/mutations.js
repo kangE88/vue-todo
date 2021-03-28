c3140157ae9a639e410d18503957a5bef5a06324
kang <dkemfjqek@gmail.com>
2021-03-28 16:18:47 +0900

    commit mapMutation

---
+++ src/store/mutations.js
@@ -0,0 +1,25 @@
+const addOneItem = (state, todoItem)  => {
+    const obj = { completed: false, item: todoItem };
+    localStorage.setItem(todoItem, JSON.stringify(obj));
+    state.todoItems.push(obj);
+}
+
+const removeOneItem = (state, payload) => {
+    //console.log(JSON.stringify(payload));
+    localStorage.removeItem(payload.todoItem.item);
+    state.todoItems.splice(payload.index, 1);
+}
+
+const toggleOneItem = (state, payload) => {
+    //todoItem.completed = !todoItem.completed;
+    state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
+    localStorage.removeItem(payload.todoItem.item);
+    localStorage.setItem(payload.todoItem.item, JSON.stringify(payload));
+}
+
+const clearAllItems = (state) => {
+    localStorage.clear();
+    state.todoItems = [];
+}
+
+export { addOneItem, removeOneItem, toggleOneItem, clearAllItems }
--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+//module 1
+//import * as getters from './getters'
+//import * as mutations from './mutations'
+
+//module 2
+import todoApp from './modules/todoApp'
 
 Vue.use(Vuex);
 
-const storage = {
-    fetch() {
-        const arr = [];
-        if(localStorage.length > 0){
-            for (let i = 0; i < localStorage.length; i++) {
-                if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
-                arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-                }
-            }
-        }
-        return arr;
-    },
-}
+// const storage = {
+//     fetch() {
+//         const arr = [];
+//         if(localStorage.length > 0){
+//             for (let i = 0; i < localStorage.length; i++) {
+//                 if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
+//                 arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+//                 }
+//             }
+//         }
+//         return arr;
+//     },
+// }
 
 export const store = new Vuex.Store({
+
+  modules: {
+    todoApp
+  }
+
          //
-         state: {
-           todoItems: storage.fetch(),
-         },
-         getters: {
+        //  state: {
+        //    todoItems: storage.fetch(),
+        //  },
+         //module1 getters.js로 이동
+         //getters, //getters: getters, 
+         
+         /* 
+         {
            storedTodoItems(state) {
              return state.todoItems;
            }
          },
-         mutations: {
+         */
+
+         //module 1 mutations.js 로 이동
+         //mutations, //mutations: mutations,
+
+         /* 
+         {
            addOneItem(state, todoItem) {
              const obj = { completed: false, item: todoItem };
              localStorage.setItem(todoItem, JSON.stringify(obj));
@@ -49,5 +70,6 @@ export const store = new Vuex.Store({
              state.todoItems = [];
            },
          },
+         */
        });
 
